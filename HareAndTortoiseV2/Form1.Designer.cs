55f5e38979255397057efaa4cce579d35a1bb940
generictecnician <generictechnician01@gmail.com>
2017-10-03 21:26:36 +0100

    Final Version

--- HareAndTortoiseV2/Form1.Designer.cs
+++ HareAndTortoiseV2/Form1.Designer.cs
@@ -33,21 +33,18 @@
             this.numDistance = new System.Windows.Forms.NumericUpDown();
             this.btnRace = new System.Windows.Forms.Button();
             this.lblOut = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.newWinnerFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.openWinnerFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.aZToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.winsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rtbRace = new System.Windows.Forms.RichTextBox();
+            this.lblOutFileInfo = new System.Windows.Forms.Label();
+            this.btnAZ = new System.Windows.Forms.Button();
+            this.btnWins = new System.Windows.Forms.Button();
+            this.rtbWinners = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.numDistance)).BeginInit();
-            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblTitle
             // 
             this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.lblTitle.Location = new System.Drawing.Point(12, 37);
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
             this.lblTitle.Name = "lblTitle";
             this.lblTitle.Size = new System.Drawing.Size(302, 23);
             this.lblTitle.TabIndex = 1;
@@ -57,7 +54,7 @@
             // lblDistance
             // 
             this.lblDistance.Cursor = System.Windows.Forms.Cursors.Default;
-            this.lblDistance.Location = new System.Drawing.Point(84, 76);
+            this.lblDistance.Location = new System.Drawing.Point(79, 47);
             this.lblDistance.Name = "lblDistance";
             this.lblDistance.Size = new System.Drawing.Size(86, 18);
             this.lblDistance.TabIndex = 0;
@@ -65,7 +62,7 @@
             // 
             // numDistance
             // 
-            this.numDistance.Location = new System.Drawing.Point(176, 74);
+            this.numDistance.Location = new System.Drawing.Point(171, 45);
             this.numDistance.Maximum = new decimal(new int[] {
             10000,
             0,
@@ -87,9 +84,9 @@
             // 
             // btnRace
             // 
-            this.btnRace.Location = new System.Drawing.Point(218, 236);
+            this.btnRace.Location = new System.Drawing.Point(14, 292);
             this.btnRace.Name = "btnRace";
-            this.btnRace.Size = new System.Drawing.Size(96, 54);
+            this.btnRace.Size = new System.Drawing.Size(297, 54);
             this.btnRace.TabIndex = 11;
             this.btnRace.Text = "Race";
             this.btnRace.UseVisualStyleBackColor = true;
@@ -97,93 +94,84 @@
             // 
             // lblOut
             // 
-            this.lblOut.Location = new System.Drawing.Point(20, 236);
+            this.lblOut.Location = new System.Drawing.Point(14, 235);
             this.lblOut.Name = "lblOut";
-            this.lblOut.Size = new System.Drawing.Size(192, 54);
+            this.lblOut.Size = new System.Drawing.Size(297, 54);
             this.lblOut.TabIndex = 12;
             this.lblOut.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // richTextBox1
+            // rtbRace
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(17, 112);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(297, 105);
-            this.richTextBox1.TabIndex = 13;
-            this.richTextBox1.Text = "";
+            this.rtbRace.Location = new System.Drawing.Point(17, 71);
+            this.rtbRace.Name = "rtbRace";
+            this.rtbRace.ReadOnly = true;
+            this.rtbRace.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.rtbRace.Size = new System.Drawing.Size(297, 161);
+            this.rtbRace.TabIndex = 13;
+            this.rtbRace.Text = "";
             // 
-            // menuStrip1
+            // lblOutFileInfo
             // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(334, 24);
-            this.menuStrip1.TabIndex = 16;
-            this.menuStrip1.Text = "menuStrip1";
+            this.lblOutFileInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutFileInfo.Location = new System.Drawing.Point(14, 367);
+            this.lblOutFileInfo.Name = "lblOutFileInfo";
+            this.lblOutFileInfo.Size = new System.Drawing.Size(297, 35);
+            this.lblOutFileInfo.TabIndex = 14;
+            this.lblOutFileInfo.Text = "How should the winners be sorted?";
+            this.lblOutFileInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // fileToolStripMenuItem
+            // btnAZ
             // 
-            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.newWinnerFileToolStripMenuItem,
-            this.openWinnerFileToolStripMenuItem});
-            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.fileToolStripMenuItem.Text = "File";
+            this.btnAZ.Location = new System.Drawing.Point(17, 405);
+            this.btnAZ.Name = "btnAZ";
+            this.btnAZ.Size = new System.Drawing.Size(145, 33);
+            this.btnAZ.TabIndex = 15;
+            this.btnAZ.Text = "A to Z";
+            this.btnAZ.UseVisualStyleBackColor = true;
+            this.btnAZ.Click += new System.EventHandler(this.btnAZ_Click);
             // 
-            // newWinnerFileToolStripMenuItem
+            // btnWins
             // 
-            this.newWinnerFileToolStripMenuItem.Name = "newWinnerFileToolStripMenuItem";
-            this.newWinnerFileToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
-            this.newWinnerFileToolStripMenuItem.Text = "New Winner File";
-            this.newWinnerFileToolStripMenuItem.Click += new System.EventHandler(this.newWinnerFileToolStripMenuItem_Click);
+            this.btnWins.Location = new System.Drawing.Point(168, 405);
+            this.btnWins.Name = "btnWins";
+            this.btnWins.Size = new System.Drawing.Size(143, 33);
+            this.btnWins.TabIndex = 16;
+            this.btnWins.Text = "Number of Wins";
+            this.btnWins.UseVisualStyleBackColor = true;
+            this.btnWins.Click += new System.EventHandler(this.btnWins_Click);
             // 
-            // openWinnerFileToolStripMenuItem
+            // rtbWinners
             // 
-            this.openWinnerFileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.aZToolStripMenuItem,
-            this.winsToolStripMenuItem});
-            this.openWinnerFileToolStripMenuItem.Name = "openWinnerFileToolStripMenuItem";
-            this.openWinnerFileToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
-            this.openWinnerFileToolStripMenuItem.Text = "Open Winner File";
-            // 
-            // aZToolStripMenuItem
-            // 
-            this.aZToolStripMenuItem.Name = "aZToolStripMenuItem";
-            this.aZToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
-            this.aZToolStripMenuItem.Text = "A to Z";
-            this.aZToolStripMenuItem.Click += new System.EventHandler(this.aZToolStripMenuItem_Click);
-            // 
-            // winsToolStripMenuItem
-            // 
-            this.winsToolStripMenuItem.Name = "winsToolStripMenuItem";
-            this.winsToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
-            this.winsToolStripMenuItem.Text = "Wins";
-            this.winsToolStripMenuItem.Click += new System.EventHandler(this.winsToolStripMenuItem_Click);
+            this.rtbWinners.Location = new System.Drawing.Point(17, 451);
+            this.rtbWinners.Name = "rtbWinners";
+            this.rtbWinners.ReadOnly = true;
+            this.rtbWinners.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.rtbWinners.Size = new System.Drawing.Size(294, 138);
+            this.rtbWinners.TabIndex = 17;
+            this.rtbWinners.Text = "";
             // 
             // Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 299);
-            this.Controls.Add(this.richTextBox1);
+            this.ClientSize = new System.Drawing.Size(334, 601);
+            this.Controls.Add(this.rtbWinners);
+            this.Controls.Add(this.btnWins);
+            this.Controls.Add(this.btnAZ);
+            this.Controls.Add(this.lblOutFileInfo);
+            this.Controls.Add(this.rtbRace);
             this.Controls.Add(this.lblOut);
             this.Controls.Add(this.btnRace);
             this.Controls.Add(this.numDistance);
             this.Controls.Add(this.lblDistance);
             this.Controls.Add(this.lblTitle);
-            this.Controls.Add(this.menuStrip1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form";
             this.Text = "The Hare And The Tortoise Racing";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numDistance)).EndInit();
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -193,13 +181,11 @@
         private System.Windows.Forms.NumericUpDown numDistance;
         private System.Windows.Forms.Button btnRace;
         private System.Windows.Forms.Label lblOut;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem newWinnerFileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem openWinnerFileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem aZToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem winsToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox rtbRace;
+        private System.Windows.Forms.Label lblOutFileInfo;
+        private System.Windows.Forms.Button btnAZ;
+        private System.Windows.Forms.Button btnWins;
+        private System.Windows.Forms.RichTextBox rtbWinners;
     }
 }
 
--- HareAndTortoiseV2/Form1.cs
+++ HareAndTortoiseV2/Form1.cs
@@ -26,69 +26,43 @@ namespace HareAndTortoiseV2
         private void Form1_Load(object sender, EventArgs e)//Get file info on application Load
         {
             int text = 0;//for text handling
-           // try {
-                if (File.Exists(fileName))
+            if (File.Exists(fileName))
+            {
+                IEnumerable<string> fileContents = File.ReadLines(fileName);//Get contents of file
+                foreach (string i in fileContents)
                 {
-                    IEnumerable<string> fileContents = File.ReadLines(fileName);//Get contents of file
-                    foreach (string i in fileContents)
+                    //ERROR CHECKING HERE NEEDS!!!!
+                    if (i.Contains("name"))
                     {
-                        //ERROR CHECKING HERE NEEDS!!!!
-                        if (i.Contains("name"))
-                        {
-                            try
-                            {
-                                names.Add(i.Substring(6));//add name sections to list, remove "<name>"
-                            }
-                            catch (Exception)
-                            {
-
-                                throw;
-                            }
-                            
-                        }
-                        else if (i.Contains("minS"))
-                        {
-                            text = Int16.Parse(i.Substring(6));//get text as a number, remove "<minS>"
-                            min.Add(text);//add speed sections to list
-                        }
-                        else if (i.Contains("maxS"))
-                        {
-                            text = Int16.Parse(i.Substring(6));// get text as a number, remove "<maxS>"
-                            max.Add(text);//add minspeed to list
-                        }
-                        else if (i.Contains("endu"))
-                        {
-                            text = Int16.Parse(i.Substring(6));//get text as a number, remove "<endu>"
-                            endurance.Add(text);//add endurance sections to list
-
-                        }
+                        names.Add(i.Substring(6));//add name sections to list, remove "<name>"
                     }
-                    noOfRacers = names.Count;
-                    wins = new int[noOfRacers];
+                    else if (i.Contains("minS"))
+                    {
+                        text = Int16.Parse(i.Substring(6));//get text as a number, remove "<minS>"
+                        min.Add(text);//add speed sections to list
+                    }
+                    else if (i.Contains("maxS"))
+                    {
+                        text = Int16.Parse(i.Substring(6));// get text as a number, remove "<maxS>"
+                        max.Add(text);//add minspeed to list
+                    }
+                    else if (i.Contains("endu"))
+                    {
+                        text = Int16.Parse(i.Substring(6));//get text as a number, remove "<endu>"
+                        endurance.Add(text);//add endurance sections to list
+
+                    }
                 }
-                else
-                {
-                    toLoad = false;
-                    string message = "No file found, Please create one using the Character Generator program. " + Environment.NewLine + "The program will close when you try to Race.";
-                    const string title = "Error! File not found";
-                    MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Question);
-                }
-            /*}
-            catch (FileNotFoundException exception )
+                noOfRacers = names.Count;
+                wins = new int[noOfRacers];
+            }
+            else
             {
                 toLoad = false;
                 string message = "No file found, Please create one using the Character Generator program. " + Environment.NewLine + "The program will close when you try to Race.";
                 const string title = "Error! File not found";
                 MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Question);
             }
-            catch (Exception exception)
-            {
-
-            }
-            finally
-            {
-                this.TopMost = true;
-            }*/
             
         }
 
@@ -102,7 +76,6 @@ namespace HareAndTortoiseV2
                 distance = (int)numDistance.Value;//get the distance of the race from the user
                 if (names.Count == min.Count && min.Count == max.Count && max.Count == endurance.Count)//Check that all lists have the same value if possible... 
                 {
-
                     Race(distance);//RACE
                 }
                 else
@@ -122,6 +95,7 @@ namespace HareAndTortoiseV2
         private void Race(int distance)
         {
             int movement = 0;
+            int round = 1;
             bool winner = false;
             Random random = new Random();
             int[] location = new int[noOfRacers];
@@ -129,13 +103,14 @@ namespace HareAndTortoiseV2
             //Process
             while (winner == false)
             {
+                rtbRace.AppendText(Environment.NewLine + "Round " + round + Environment.NewLine);
                 for (int i = 0; i < noOfRacers; i++)
                 {
                     if (endurance[i] > 0)
                     {
                         movement = random.Next(min[i], max[i]);
                         location[i] += movement;
-                        endurance[i] -= movement;
+                        endurance[i] -= movement;  
                     }
                     else
                     {
@@ -144,35 +119,72 @@ namespace HareAndTortoiseV2
                     if (location[i] >= distance)
                     {
                         winner = true;
-                        lblOut.Text = names[i];
+                        lblOut.Text = "The winner is the " + names[i] + "!";
                         wins[i]++;
                     }
+                    rtbRace.AppendText(names[i] + ": " + location[i] + Environment.NewLine);
                 }
+                round++;
             }
         }
 
-        private void newWinnerFileToolStripMenuItem_Click(object sender, EventArgs e)
+        private void btnAZ_Click(object sender, EventArgs e)
         {
-            //TODO NEW FILES
-        }
+            //Title
+            File.AppendAllText("Wins.txt", Environment.NewLine + "Sorting by Wins" + Environment.NewLine);
+            rtbWinners.AppendText(Environment.NewLine + "Sorting by Wins" + Environment.NewLine);
 
-        private void aZToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //SORTING WINS
-        }
+            //Sorting
+            int[] tmpWins = wins;//Doesn't effect primary array
+            string[] nameArray = names.ToArray();//array from names
+            Array.Sort(nameArray, tmpWins);//sort with each other
 
-        private void winsToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //List.Sort(names, wins); MAKE WORK
-        }
-
-        private void Form_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            //write all text to a file on close
-            File.WriteAllText("wins.info", "");
-            for (int i = 0; i < noOfRacers; i++)
+            //output names
+            foreach (var item in nameArray)
             {
-                File.AppendAllText("wins.info", names[i].ToString() + ":" + wins[i].ToString() + Environment.NewLine);
+                File.AppendAllText("Wins.txt", item + " | ");
+                rtbWinners.AppendText(item + " | ");
+            }
+
+            //line
+            File.AppendAllText("Wins.txt", Environment.NewLine);
+            rtbWinners.AppendText(Environment.NewLine);
+
+            //Other outs
+            foreach (int item in tmpWins)
+            {
+                File.AppendAllText("Wins.txt", item.ToString() + " | ");
+                rtbWinners.AppendText(item.ToString() + " | ");
+            }
+        }
+
+        private void btnWins_Click(object sender, EventArgs e)
+        { 
+            //Title
+            File.AppendAllText("Wins.txt", Environment.NewLine + "Sorting by Wins" + Environment.NewLine);
+            rtbWinners.AppendText(Environment.NewLine + "Sorting by Wins" + Environment.NewLine);
+
+            //Sorting
+            int[] tmpWins = wins;//Doesn't effect primary array
+            string[] nameArray = names.ToArray();//array from names
+            Array.Sort(tmpWins, nameArray);//sort with each other
+
+            //output names
+            foreach (var item in nameArray)
+            {
+                File.AppendAllText("Wins.txt", item + " | ");
+                rtbWinners.AppendText(item + " | ");
+            }
+
+            //line
+            File.AppendAllText("Wins.txt", Environment.NewLine);
+            rtbWinners.AppendText(Environment.NewLine);
+
+            //Other outs
+            foreach (int item in tmpWins)
+            {
+                File.AppendAllText("Wins.txt", item.ToString() + " | ");
+                rtbWinners.AppendText(item.ToString() + " | ");
             }
         }
     }
